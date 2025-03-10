021d2985ecbb166c8dee98a0ffb1016539ddfce2
austinechisutia <austineakhonya624@gmail.com>
2025-03-10 14:11:16 +0300

    working on it

--- shop.js
+++ shop.js
@@ -6,52 +6,63 @@ let containerHTML = '';
 
 images.forEach((image)=>{
     containerHTML += `
-        <div class="pro-id" name=${image.id}>
-          <div class="product-small">
-            <div class="product-small-image js-product-small-image">  
-              <img class="js-image" src="images/green/5.jpeg" alt="">
-            </div>
-            
+        <div class="product-small" name="product-small-${image.id}">
+          <div class="product-small-image js-product-small-image">
+            <img src="${image.imagesContainerOne.image7}" alt="">
           </div>
-          <div class="product-image">
-            <div class="image-product-container">
-              <img src="${image.imagesContainerOne.image1}" alt="">
-            </div>
+          <div class="product-small-image js-product-small-image">
+            <img src="${image.imagesContainerOne.image2}" alt="">
           </div>
-          <div class="product-content">
-            <div class="product-title">Nike Sportswear Club</div>
-            <div class="product-subtitle">
-              Men's T-Shirt
-            </div>
-            <div class="product-price">€24.99</div>
-            <div class="product-content-image">
-              <div class="product-content-image-one js-product-content-image-one">
-                <img src="${image.imagesContainerOne.image1}" alt="">
-                <img src="${image.imagesContainerOne.image2}" alt="">
-                <img src="${image.imagesContainerOne.image3}" alt="">
-                <img src="${image.imagesContainerOne.image4}" alt="">
-                <img src="${image.imagesContainerOne.image5}" alt="">
-              </div>
-            </div>
-            <div class="product-sizes">
-              <div class="product-content-size-title">
-                <div class="product-content-size-title-one">Select Size</div>
-                <div class="product-content-size-title-two">Size Guide</div>
-  
-              </div>
-              <div class="product-sizes-specific">
-                <button class="bx">XS</button>
-                <button class="bx">S</button>
-                <button class="bx">M</button>
-                <button class="bx">L</button>
-                <button class="bx">XL</button>
-  
-              </div>
-              <button class="product-sizes-button main">Add to Bag</button>
-              <button class="product-sizes-button sub-main">Favourite</button>
-  
+          <div class="product-small-image js-product-small-image">
+            <img src="${image.imagesContainerOne.image3}" alt="">
+          </div>
+          <div class="product-small-image js-product-small-image">
+            <img src="${image.imagesContainerOne.image1}" alt="">
+          </div>
+          <div class="product-small-image js-product-small-image">
+            <img src="${image.imagesContainerOne.image5}" alt="">
+          </div>
+          <div class="product-small-image js-product-small-image">
+            <img src="${image.imagesContainerOne.image6}" alt="">
+          </div>
+          
+          
+        </div>
+        <div class="product-image">
+          <div class="image-product-container js-image-product-container">
+            <img src="${image.imagesContainerOne.image1}" alt="">
+          </div>
+        </div>
+        <div class="product-content">
+          <div class="product-title">Nike Sportswear Club</div>
+          <div class="product-subtitle">
+            Men's T-Shirt
+          </div>
+          <div class="product-price">€24.99</div>
+          <div class="product-content-image">
+            <div class="product-content-image-one js-product-content-image-one">
+
             </div>
           </div>
+          <div class="product-sizes">
+            <div class="product-content-size-title">
+              <div class="product-content-size-title-one">Select Size</div>
+              <div class="product-content-size-title-two">Size Guide</div>
+
+            </div>
+            <div class="product-sizes-specific">
+              <button class="bx">XS</button>
+              <button class="bx">S</button>
+              <button class="bx">M</button>
+              <button class="bx">L</button>
+              <button class="bx">XL</button>
+
+            </div>
+            <button class="product-sizes-button main">Add to Bag</button>
+            <button class="product-sizes-button sub-main">Favourite</button>
+
+          </div>
+        </div>
     `
 });
     
@@ -61,14 +72,14 @@ container.innerHTML = containerHTML;
 document.addEventListener("DOMContentLoaded", () => {
   const productSmall = document.querySelectorAll('.js-product-small-image');
   const productImage = document.querySelector('.js-image-product-container img');
-  const jsImage = document.querySelectorAll('.c');  // error here
+  const jsImage = document.querySelectorAll('.js-image');  // error here
 
 
   if (productSmall.length > 0 && productImage) {
       productSmall.forEach((smallImage) => {
-          smallImage.addEventListener('mouseover', () => {
-              const smallImageSrc = smallImage.querySelector('img').getAttribute('src');
-              productImage.setAttribute('src', smallImageSrc);
+            smallImage.addEventListener('mouseover', () => {
+              const newSrc = smallImage.tagName === 'IMG' ? smallImage.src : smallImage.querySelector('img').src;
+              productImage.src = newSrc;
           });
       });
   }
@@ -83,6 +94,13 @@ const imageHTML = images.map((image)=>{
 }).join('');
 
 producuctContainer.innerHTML = imageHTML;
+
+
+producuctContainer.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'IMG'){
+        document.querySelector('.js-image-product-container img').src = e.target.src;
+    }
+});
 
 
 
